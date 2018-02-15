1c1810a3aaa5b761d4ee57c2bdba2a2a4373b462
noble0423 <ryan.noble82@gmail.com>
2018-02-15 14:59:04 -0800

    added variables and array to log user input

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,48 +1,21 @@
 // find a way to stop the game after 10 incorrect answers (for loop maybe). 10 incorrect answers equals 1 loss
     // restart game after a win
 
-// display guesses made until win/loss...return(userGuess) maybe?
-
-// make computer guess stick (right now it changes to a random letter everytime i press a new key...maybe this is the return??)
-
-
-
-
+// display guesses made until win/loss in HTML
 
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
+var guessesLeft = 10;
+var guessedLetters = [];
 
 var computerGuess = options[Math.floor(Math.random()*options.length)];
     console.log("Computer picked " + computerGuess);
 
-//function makeRandomNumber() {
-//   return Math.floor(Math.random() * 10);
-//  }
-
-//function printRandomNumber() {
-//   console.log(Math.floor(Math.random() * 10));
-//  }
-
-//function makeRandomLetter(options) {
-//   return Math.floor(Math.random() * 10);
-//  }
-
-
-//for (i = 0; i = 9; i++) {}
 document.onkeyup = function() {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        console.log("User guessed " + userGuess);
-           
-    //if (userGuess === options[i]) {
-    //   console.log("great, letter picked");
-    //}
-
-    //else {
-    //    console.log("key other than a letter was picked");
-    //}
-
+        guessedLetters.push(userGuess);
+        console.log(guessedLetters);
      
     if (options.indexOf(userGuess) === -1) {
         console.log("non letter was picked");
@@ -59,19 +32,20 @@ document.onkeyup = function() {
         }
 
         else {
+            //return;
             console.log("That's not the letter I am thinking of, hehehe");
             console.log(guessesLeft--);
         }
     }
 
-    // could a for loop end the game somehow?? for (i = 0; i < 10; i++) {}...maybe this could encompass the whole game and it stops when it has run 10 x's, unless user guesses correctly.
-    if (guessesLeft === -1) {
+    if (guessesLeft === 0) {
+        losses++;
         console.log("STOP GAME");
     }
 
-    var html = "<h1>Welcome to the Psychic Game!!!!</h1>" + "<p>I am thinking of a letter...</p>" + "<p>type any letter to see if you are psychic.</p>" + "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses left: " + guessesLeft + "</p>" + "<p>Letters you have guessed so far: " + "</p>"; 
+    var html = "<h1>Welcome to the Psychic Game!!!!</h1>" + "<p>I am thinking of a letter...</p>" + "<p>type any letter to see if you are psychic.</p>" + "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses left: " + guessesLeft + "</p>" + "<p>Letters you have guessed so far: " + userGuess + "</p>"; 
 
-            document.querySelector("#game").innerHTML = html;
+        document.querySelector("#game").innerHTML = html;
         
 
     
