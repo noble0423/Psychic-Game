37f8ec853b720d3948b5b5d36a4e4ae4c742c779
noble0423 <ryan.noble82@gmail.com>
2018-02-16 00:20:40 -0800

    submission for HW. Still need to complete 1. rewriting the screen after win/loss, 2. computer pick a new letter after win/loss, 3. ensure user only selects letters

---
+++ Assets/javascript/game2.js
@@ -0,0 +1,69 @@
+
+
+// VARIABLES
+// ==========================================================================
+
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessedLetters = [];
+var wrongGuessSayings = ["That's not the letter I am thinking of, hehehe.", "BUZZ!--nope!", "Nice try, but not correct.", "You're no Psychic", "You'll never guess the letter that I am thinking", "I can see we're gonna be here for a while", "This really isn't that hard", "Wrong! BWAHAHAHAHAHA!"];
+
+
+
+
+// FUNCTIONS
+// ==============================================================================
+// Function to render Random letter chosen by computer
+function computerPick() {
+    var computerGuess = options[Math.floor(Math.random()*options.length)];
+        console.log("Computer's letter is " + computerGuess);
+}
+// Function to capture letter chosen by user
+function userPick() {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+        guessedLetters.push(userGuess);
+        console.log("User picked letter " + guessedLetters);
+}        
+//}
+// Function that updates the score...
+
+
+// MAIN PROCESS
+// ==============================================================================
+// Calling functions to start the game.
+computerPick();
+
+document.onkeyup = function(event) {
+    userPick();
+
+    // If all guesses have been made, stop the function.
+    if (guessesLeft === 0) {
+        return;
+    }
+    // Determine which key was pressed, make it lowercase, and set it to the userGuess variable.
+    
+    
+    
+}
+    
+    //if (options.indexOf(psychicGame.userGuess) === -1) {
+    //console.log("non letter was picked");
+    //   alert("I am thinking of a letter! Numbers and other characters don't count! //A true Psychic would know that.");
+        
+    //}
+
+    if (computerPick() === userPick()) { 
+        alert("Oh my god--I guess you are psychic!");
+        wins++;
+    }
+
+    //else {
+    //    var youAreWrong = wrongGuessSayings[Math.floor(Math.random()//*wrongGuessSayings.length)];
+    //        alert(youAreWrong);
+    //        console.log(guessesLeft--);
+    //}
+
+
+//}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Psychic-Game
+
+### Overview
+
+In this game, "Psychic Game", you'll try to guess what letter the computer is thinking. If you guess correctly, you are a psychic.
+
+### How the game works
+
+1. The computer will pick randomly pick a letter.
+
+2. By using your keyboard (and your psychic abilities), select the letter that you think will match the computer's.
+
+3. The computer will give you no more than 10 guesses in order to determine if you are a psychic.
+
+4. Answer correctly, and you win! Your win total will increase on the screen as well.
+
+5. Exhaust all 10 guesses, and you lose. Your loss total will increase on the screen as well.
+
+    * Don't worry about trying to remember what letters you've already guessed, the computer is nice enough to display those for you on the screen.
+
+    * How will you know if you guessed correctly?? The computer will prompt you letting you know if your guess was right or wrong. 
+
+**Good luck!**
