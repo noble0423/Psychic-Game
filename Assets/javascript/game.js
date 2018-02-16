5095794d1f4e5fa70e404bec02fe089c9d60e2e4
noble0423 <ryan.noble82@gmail.com>
2018-02-16 00:59:40 -0800

    saving again. when i pull up the link through github, there is no javascript

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -7,7 +7,7 @@
 
 // don't allow user to pick same letter they have already picked
 
-//VARIABLES
+//VARIABLES 
 // =========================================================================================
 
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
--- Assets/javascript/game2.js
+++ Assets/javascript/game2.js
@@ -6,7 +6,7 @@
 var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
+var guessesLeft = 10; 
 var guessedLetters = [];
 var wrongGuessSayings = ["That's not the letter I am thinking of, hehehe.", "BUZZ!--nope!", "Nice try, but not correct.", "You're no Psychic", "You'll never guess the letter that I am thinking", "I can see we're gonna be here for a while", "This really isn't that hard", "Wrong! BWAHAHAHAHAHA!"];
 
